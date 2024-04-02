a8535ba9e9ce31540207649ae135fab6ddd179c9
MarlonBuosi <marlon.v.buosi@eexonmobil.com>
2024-04-01 22:04:41 -0300

    test e2e working

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,40 +1,38 @@
-import { randomUUID } from 'crypto'
-import { Environment } from 'vitest'
-import 'dotenv/config'
-import { execSync } from 'node:child_process'
-import { prisma } from '@/lib/prisma'
+/* eslint-disable prettier/prettier */
+import { randomUUID } from "node:crypto";
+import { Environment } from "vitest";
+import "dotenv/config";
+import { execSync } from "node:child_process";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 function generateDatabaseURL(schema: string) {
   if (!process.env.DATABASE_URL) {
-    throw new Error('Please provide a DATABASE_URL environment variable')
+    throw new Error("Forneça uma variável de ambiente DATABASE_URL!");
   }
 
-  const url = new URL(process.env.DATABASE_URL)
-
-  url.searchParams.set('schema', schema)
-
-  return url.toString()
+  const url = new URL(process.env.DATABASE_URL);
+  url.searchParams.set('schema', schema);
+  return url.toString();
 }
 
 export default <Environment>{
-  name: 'prisma',
-  setup: async () => {
-    const schema = randomUUID()
-    const databaseURL = generateDatabaseURL(schema)
+  name: "prisma",
+  transformMode: "ssr",
+  async setup() {
+    const schema = randomUUID();
+    const databaseUrl = generateDatabaseURL(schema);
 
-    process.env.DATABASE_URL = databaseURL
+    process.env.DATABASE_URL = databaseUrl;
 
-    execSync('npx prisma migrate deploy')
+    execSync("npx prisma migrate deploy");
 
     return {
       async teardown() {
-        await prisma.$executeRawUnsafe(
-          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
-        )
-
-        await prisma.$disconnect()
+        await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schema}" CASCADE`);
+        await prisma.$disconnect();
       },
-    }
+    };
   },
-  transformMode: 'ssr',
-}
+};
