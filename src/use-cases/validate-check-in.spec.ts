8e37146596a3d7fbb583a5087d9b3355e70c622c
MarlonBuosi <marlonbuosi_@hotmail.com>
2024-06-26 17:57:24 -0300

    fix unit test

--- src/use-cases/validate-check-in.spec.ts
+++ src/use-cases/validate-check-in.spec.ts
@@ -1,34 +1,15 @@
-import { expect, describe, it, beforeEach, afterEach, vi } from 'vitest'
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
-import { ValidateCheckinUseCase } from './validate-check-in'
-import { ResourceNotFoundError } from './errors/resource-not-found-error'
-// Test unitario NUNCA toca em banco de dados, nem em camadas externas da aplicacao
+import { ResourceNotFoundError } from '@/use-cases/errors/resource-not-found-error'
+import { expect, describe, it, beforeEach, afterEach, vi } from 'vitest'
+import { ValidateCheckInUseCase } from './validate-check-in'
 
 let checkInsRepository: InMemoryCheckInsRepository
-let sut: ValidateCheckinUseCase
+let sut: ValidateCheckInUseCase
 
-describe('validate check in Use Case', () => {
-  beforeEach(() => {
+describe('Validate Check-in Use Case', () => {
+  beforeEach(async () => {
     checkInsRepository = new InMemoryCheckInsRepository()
-    sut = new ValidateCheckinUseCase(checkInsRepository)
-
-    // gymsRepository.items.push({
-    //   id: 'gym-01',
-    //   title: 'JavaScript Gym',
-    //   description: '',
-    //   phone: '',
-    //   latitude: new Decimal(-27.2092052),
-    //   longitude: new Decimal(-49.6401091),
-    // })
-
-    // gymsRepository.create({
-    //   id: 'gym-01',
-    //   title: 'JavaScript Gym',
-    //   description: '',
-    //   phone: '',
-    //   latitude: -27.2092052,
-    //   longitude: -49.6401091,
-    // })
+    sut = new ValidateCheckInUseCase(checkInsRepository)
 
     vi.useFakeTimers()
   })
@@ -37,10 +18,10 @@ describe('validate check in Use Case', () => {
     vi.useRealTimers()
   })
 
-  it('should be able to validate check in', async () => {
+  it('should be able to validate the check-in', async () => {
     const createdCheckIn = await checkInsRepository.create({
-      gym_id: 'gym_01',
-      user_id: 'user_01',
+      gym_id: 'gym-01',
+      user_id: 'user-01',
     })
 
     const { checkIn } = await sut.execute({
@@ -51,27 +32,27 @@ describe('validate check in Use Case', () => {
     expect(checkInsRepository.items[0].validated_at).toEqual(expect.any(Date))
   })
 
-  it('should not be able to validate an inexistent check in', async () => {
+  it('should not be able to validate an inexistent check-in', async () => {
     await expect(() =>
       sut.execute({
-        checkInId: 'inexistent id',
+        checkInId: 'inexistent-check-in-id',
       }),
     ).rejects.toBeInstanceOf(ResourceNotFoundError)
   })
 
-  it('should not be able to validate an inexistent check-in after 20 minutes of its creation', async () => {
+  it('should not be able to validate the check-in after 20 minutes of its creation', async () => {
     vi.setSystemTime(new Date(2023, 0, 1, 13, 40))
 
     const createdCheckIn = await checkInsRepository.create({
-      gym_id: 'gym_01',
-      user_id: 'user_01',
+      gym_id: 'gym-01',
+      user_id: 'user-01',
     })
 
     const twentyOneMinutesInMs = 1000 * 60 * 21
 
     vi.advanceTimersByTime(twentyOneMinutesInMs)
 
-    expect(() =>
+    await expect(() =>
       sut.execute({
         checkInId: createdCheckIn.id,
       }),
